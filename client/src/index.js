8f44c9c6f713f6d327cf1a8e0e595b4391086907
Dillon McQuade <dillonkmcquade@gmail.com>
2023-06-29 15:28:49 -0400

    Add UserProvider to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { MenuProvider } from "./context/MenuContext";
+import { UserProvider } from "./context/UserContext";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <MenuProvider>
-        <App />
-      </MenuProvider>
+      <UserProvider>
+        <MenuProvider>
+          <App />
+        </MenuProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
