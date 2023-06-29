6f7826b67808e8ab830eaad31a0bfc8425ff72ee
Dillon McQuade <dillonkmcquade@gmail.com>
2023-06-29 10:42:17 -0400

    Add react-router-dom and routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,13 @@
+import Home from "./pages/Home.jsx";
+import { Routes, Route } from "react-router-dom";
+import { GlobalStyles } from "./GlobalStyles.js";
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
