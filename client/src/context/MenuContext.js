ab94fa8a968760bed56918964459c9647de8626e
Dillon McQuade <dillonkmcquade@gmail.com>
2023-07-03 16:43:41 -0400

    Remove console.logs

--- client/src/context/MenuContext.js
+++ client/src/context/MenuContext.js
@@ -1,12 +1,9 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 
 export const MenuContext = createContext(null);
 
 export const MenuProvider = ({ children }) => {
   const [menuVisible, setMenuVisible] = useState(false);
-  useEffect(() => {
-    console.log("Menu visible: ", menuVisible);
-  }, [menuVisible]);
   return (
     <MenuContext.Provider value={{ menuVisible, setMenuVisible }}>
       {children}
