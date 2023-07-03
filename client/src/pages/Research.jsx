01505431663198dd0c452fcd1c222b8f1b582d8c
Dillon McQuade <dillonkmcquade@gmail.com>
2023-07-03 14:24:13 -0400

    Add keyboard navigation to dropdown

--- client/src/pages/Research.jsx
+++ client/src/pages/Research.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import {
   Alert,
   AlertTitle,
@@ -6,6 +6,7 @@ import {
   IconButton,
   TextField,
 } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 import SearchIcon from "@mui/icons-material/Search";
 import ClearIcon from "@mui/icons-material/Clear";
@@ -16,8 +17,42 @@ export default function Research() {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const [inputText, setInputText] = useState("");
+  const [isSelected, setIsSelected] = useState(0);
 
-  const submitSearch = async () => {
+  const navigate = useNavigate();
+  const ref = useRef(null);
+
+  //some nice to have features for selecting w/ keyboard
+  const handleKeyDown = (event) => {
+    switch (event.key) {
+      case "Enter": {
+        const result = results[isSelected];
+        navigate(result.ticker);
+        return;
+      }
+      case "ArrowUp": {
+        if (isSelected > 0) {
+          setIsSelected(isSelected - 1);
+        }
+        break;
+      }
+      case "ArrowDown": {
+        if (isSelected < results.length - 1) {
+          setIsSelected(isSelected + 1);
+        }
+        break;
+      }
+      case "Escape": {
+        clearSearch();
+        break;
+      }
+      default:
+        return;
+    }
+  };
+
+  //query to backend
+  const search = async () => {
     setLoading(true);
     try {
       const request = await fetch(`/search?name=${inputText}`);
@@ -32,50 +67,33 @@ export default function Research() {
     } catch (error) {
       setError(error.message);
       setResults(null);
+    } finally {
     }
   };
-  //only query backend once typing has stopped
 
-  const debouncedSubmit = useDebounce(submitSearch);
+  //only query backend once typing has stopped 500ms delay
+  const debouncedSearch = useDebounce(search);
 
   const handleChange = (event) => {
+    event.preventDefault();
     setInputText(event.target.value);
 
-    if (event.target.value.length > 1) {
-      debouncedSubmit();
+    if (event.target.value.length > 3) {
+      debouncedSearch();
     } else {
       setResults(null);
     }
-
     setError("");
+  };
+
+  const clearSearch = function () {
+    setResults(null);
+    setInputText("");
   };
 
   return (
     <Wrapper>
       <Title>Search Stocks</Title>
-      <SearchForm>
-        <WhiteBorderTextField
-          id="searchField"
-          variant="outlined"
-          onChange={handleChange}
-          value={inputText}
-          type="text"
-          size="small"
-          InputProps={{
-            startAdornment: <SearchIcon />,
-            endAdornment: loading ? (
-              <CircularProgress size="16px" sx={{ color: "white" }} />
-            ) : (
-              results && (
-                <IconButton onClick={() => setInputText("")}>
-                  <ClearIcon size="16px" sx={{ color: "white" }} />
-                </IconButton>
-              )
-            ),
-          }}
-          label="Search"
-        />
-      </SearchForm>
       {error && (
         <Alert
           severity="error"
@@ -90,7 +108,45 @@ export default function Research() {
           {error}
         </Alert>
       )}
-      {results && results.map((result) => <p>{result.name}</p>)}
+      <SearchForm onSubmit={(event) => event.preventDefault()}>
+        <WhiteBorderTextField
+          id="searchField"
+          variant="outlined"
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          value={inputText}
+          type="text"
+          size="small"
+          tabIndex="0"
+          autoFocus={true}
+          InputProps={{
+            startAdornment: <SearchIcon />,
+            endAdornment: loading ? (
+              <CircularProgress size="16px" sx={{ color: "white" }} />
+            ) : (
+              results && (
+                <IconButton onClick={clearSearch}>
+                  <ClearIcon size="16px" sx={{ color: "white" }} />
+                </IconButton>
+              )
+            ),
+          }}
+          label="Search"
+        />
+        {results &&
+          results.map((result, index) => (
+            <SearchResult
+              isselected={index === isSelected && "#0f1410"}
+              ref={index === isSelected ? ref : null}
+              key={Math.random()}
+              tabIndex={0}
+              onClick={() => navigate(`/research/${result.ticker}`)}
+            >
+              <Ticker>{result.ticker}</Ticker>
+              <Name>{result.name}</Name>
+            </SearchResult>
+          ))}
+      </SearchForm>
     </Wrapper>
   );
 }
@@ -106,6 +162,12 @@ const Title = styled.h1`
   margin-bottom: 1rem;
   margin-left: 1rem;
 `;
+
+const SearchForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
 const WhiteBorderTextField = styled(TextField)`
   width: 85vw;
   align-self: center;
@@ -133,7 +195,31 @@ const WhiteBorderTextField = styled(TextField)`
   }
 `;
 
-const SearchForm = styled.form`
-  display: flex;
-  flex-direction: column;
+const SearchResult = styled.div`
+  width: 85vw;
+  margin: 0 auto;
+  padding: 0.2rem;
+  border-bottom: 1px solid gray;
+  cursor: pointer;
+  background-color: ${(props) => props.isselected};
+  animation: none !important;
+  transition: none !important
+
+  &:hover {
+    background-color: #0f1410;
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Ticker = styled.div`
+  font-style: italic;
+  color: gray;
+  animation: none !important;
+  transition: none !important;
+`;
+const Name = styled.div`
+  animation: none !important;
+  transition: none !important;
 `;
