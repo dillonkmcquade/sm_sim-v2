e13f5f38aa48f223f40c7612cf8b73bff07bf47a
Dillon McQuade <dillonkmcquade@gmail.com>
2023-07-05 12:34:52 -0400

    sellStock.js

---
+++ server/handlers/sellStock.js
@@ -0,0 +1,60 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI, DB_NAME } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const sellStock = async (req, res) => {
+  const { id } = req.params;
+  const { _id, quantity, currentPrice } = req.body;
+  const client = new MongoClient(MONGO_URI, options);
+  if (!id || !currentPrice || !_id || !id) {
+    return res.status(400).json({
+      status: 400,
+      data: req.body,
+      params: id,
+      message: "missing data",
+    });
+  }
+  try {
+    await client.connect();
+    const database = client.db(DB_NAME);
+    const users = database.collection("users");
+    const user = await users.findOne({ _id });
+    if (!user) {
+      return res.status(404).json({ status: 200, message: "user not found" });
+    }
+
+    const amountToSubtract = Number(currentPrice) * quantity;
+    const update = await users.updateOne(
+      { _id },
+      {
+        $inc: {
+          balance: amountToSubtract,
+          "holdings.$[xxx].quantity": -quantity,
+        },
+      },
+      { arrayFilters: [{ "xxx.ticker": id }] }
+    );
+
+    if (update.matchedCount === 0 || update.modifiedCount === 0) {
+      return res
+        .status(400)
+        .json({ status: 400, message: "Could not update user holdings" });
+    }
+    return res.status(200).json({
+      status: 200,
+      message: "Stock successfully sold",
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ status: 500, message: "Server error" });
+  } finally {
+    client.close();
+  }
+};
+
+module.exports = { sellStock };
