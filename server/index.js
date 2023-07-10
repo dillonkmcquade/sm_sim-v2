e5e7aef189ff2b88c530c4ef0189b34cec694849
Dillon McQuade <dillonkmcquade@gmail.com>
2023-07-10 18:20:57 -0400

    allow authorization header in backend reqs

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ server
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     next();
   })
