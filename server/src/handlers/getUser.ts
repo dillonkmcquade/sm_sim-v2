e58f9a4aa798c67fdc2c4a7108078885b35bf047
Dillon McQuade <dillonkmcquade@gmail.com>
2023-07-16 14:43:38 -0400

    query by sub instead of _id

--- server/src/handlers/getUser.ts
+++ server/src/handlers/getUser.ts
@@ -1,17 +1,17 @@
-import * as mongodb from "mongodb";
 import { Response, Request } from "express";
 import { collections } from "../services/database.service";
 
 export const getUser = async (req: Request, res: Response) => {
-  const { _id } = req.params;
-  if (!_id) {
+  const id = req?.params?._id;
+  if (!req.params._id) {
     return res
       .status(400)
       .json({ status: 400, message: "No query string given" });
   }
   try {
     const { users } = collections;
-    const user = await users.findOne({ _id: new mongodb.ObjectId(_id) });
+    const query = { sub: id };
+    const user = await users.findOne(query);
     if (!user) {
       return res.status(404).json({ status: 404, message: "User not found" });
     }
@@ -20,6 +20,6 @@ export const getUser = async (req: Request, res: Response) => {
       data: user,
     });
   } catch (error) {
-    res.status(500).json({ status: 500, message: "Server error" });
+    res.status(500).json({ status: 500, message: error.message });
   }
 };
