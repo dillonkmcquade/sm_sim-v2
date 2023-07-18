38643869b0d67609b1182909a713883f65fafbd5
Dillon McQuade <dillonkmcquade@gmail.com>
2023-07-18 17:39:18 -0400

    convert usercontext

---
+++ client/src/context/UserContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, useState, useEffect, ReactNode } from "react";
+
+export interface Holding {
+  quantity: number;
+  ticker: string;
+  price: number;
+}
+
+export interface User {
+  _id: string;
+  balance: number;
+  holdings: Holding[];
+  watchList: string[];
+  nickname: string;
+  name: string;
+  picture: string;
+  updated_at?: string;
+  verified_at?: string;
+  sub:string,
+  email?: string
+}
+export interface GlobalContent {
+  currentUser: User;
+  setCurrentUser: React.Dispatch<User>; 
+}
+
+type Props = {
+  children: ReactNode;
+}
+
+export const UserContext = createContext<GlobalContent | null>(null);
+
+export const UserProvider = ({ children }: Props) => {
+  const [currentUser, setCurrentUser] = useState(() => {
+    const cached = window.sessionStorage.getItem("user");
+    if (cached) {
+      return JSON.parse(cached);
+    } else {
+      return null;
+    }
+  });
+
+  useEffect(() => {
+    window.sessionStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
