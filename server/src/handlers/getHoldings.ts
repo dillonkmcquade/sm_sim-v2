aa1ceff75477ac504accf1ca4c8bd20b89afdfb0
Dillon McQuade <dillonkmcquade@gmail.com>
2023-08-04 17:44:46 -0400

    remove console.log

--- server/src/handlers/getHoldings.ts
+++ server/src/handlers/getHoldings.ts
@@ -10,13 +10,10 @@ export async function getHoldings(req: Request, res: Response) {
       "SELECT * FROM transactions WHERE transaction_id=$1",
       [auth?.sub],
     );
-
     return res
       .status(200)
       .json({ status: 200, message: "success", data: rows });
   } catch (error) {
-    if (error instanceof Error) {
-      return console.log(error.message);
-    }
+    return res.status(500).json({ status: 500, message: "Server error" });
   }
 }
