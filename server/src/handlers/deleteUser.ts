b1760d9adc20e299e16b134a829e441dd7a07b29
Dillon McQuade <dillonkmcquade@gmail.com>
2023-08-14 09:55:42 -0400

    add transaction to deleteUser

--- server/src/handlers/deleteUser.ts
+++ server/src/handlers/deleteUser.ts
@@ -4,11 +4,17 @@ import { pool } from "../services/database.service";
 
 export const deleteUser = async (req: Request, res: Response) => {
   const sub = req.auth?.payload.sub;
+  const client = await pool.connect();
   try {
-    await pool.query("DELETE FROM transactions WHERE transaction_id=$1", [sub]);
-    await pool.query("DELETE FROM users WHERE auth0_id=$1", [sub]);
+    await client.query("BEGIN");
+    await client.query("DELETE FROM transactions WHERE transaction_id=$1", [
+      sub,
+    ]);
+    await client.query("DELETE FROM users WHERE auth0_id=$1", [sub]);
+    await client.query("COMMIT");
     return res.status(200).json({ status: 200, message: "Account deleted" });
   } catch (error) {
+    await client.query("ROLLBACK");
     return res.status(500).json({ status: 500, message: "Server error" });
   }
 };
