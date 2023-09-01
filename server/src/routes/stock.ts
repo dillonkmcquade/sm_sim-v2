c3c072e08d07827c6b0bfa1643ffc83e5b723e44
Dillon McQuade <dillonkmcquade@gmail.com>
2023-08-31 21:51:57 -0400

    finished stock router

--- server/src/routes/stock.ts
+++ server/src/routes/stock.ts
@@ -1,18 +1,106 @@
 import { Router } from "express";
-import { getQuote } from "../handlers/getQuote";
-import { getCandle } from "../handlers/getCandle";
-import { getNews } from "../handlers/getNews";
-import { StockController } from "../controllers/StockController";
-import { queryTickerByName } from "../handlers/queryTickerByName";
+import { StockController } from "../models/StockController";
 import { pool } from "../index";
 
 const stockRouter = Router();
 const stockController = new StockController(pool);
 
-stockRouter
-  .get("/news/:ticker", (req, res) => getNews(req, res, stockController))
-  .get("/quote/:ticker", (req, res) => getQuote(req, res, stockController))
-  .get("/candle/:ticker", (req, res) => getCandle(req, res, stockController))
-  .get("/search", (req, res) => queryTickerByName(req, res, stockController));
+stockRouter.get("/news/:ticker", async (req, res) => {
+  const { ticker } = req.params;
+  if (!ticker) {
+    return res.status(400).json({ status: 400, message: "Missing ticker id" });
+  }
+  try {
+    const news = await stockController.news(ticker);
+    if (news) {
+      return res.status(200).json({ status: 200, data: news });
+    } else {
+      return res.status(404).json({
+        status: 404,
+        message: "Failed request, possibly incorrect ticker symbol",
+      });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({ status: 400, message: error.message });
+    }
+    return;
+  }
+});
+
+stockRouter.get("/quote/:ticker", async (req, res) => {
+  const { ticker } = req.params;
+  if (!ticker) {
+    return res.status(400).json({ status: 400, message: "Missing ticker id" });
+  }
+  try {
+    const quote = await stockController.quote(ticker);
+    if (quote) {
+      return res.status(200).json({ status: 200, data: quote });
+    } else {
+      return res.status(404).json({
+        status: 404,
+        message: "Failed request, possibly incorrect ticker symbol",
+      });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({ status: 400, message: error.message });
+    }
+    return;
+  }
+});
+
+stockRouter.get("/candle/:ticker", async (req, res) => {
+  const { ticker } = req.params;
+  const { resolution, from } = req.query;
+  if (!ticker || !resolution || !from) {
+    return res.status(400).json({ status: 400, message: "Missing ticker id" });
+  }
+  try {
+    const candle = await stockController.candle(
+      ticker,
+      resolution.toString(),
+      from.toString(),
+    );
+    if (candle) {
+      return res.status(200).json({ status: 200, data: candle });
+    } else {
+      return res.status(404).json({
+        status: 404,
+        message: "Failed request, possibly incorrect ticker symbol",
+      });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({ status: 400, message: error.message });
+    }
+    return;
+  }
+});
+
+stockRouter.get("/search", async (req, res) => {
+  const name = req.query.name as string;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ status: 400, message: "No query string given" });
+  }
+  try {
+    const results = await stockController.search(name);
+    if (!results) {
+      return res.status(400).json({ status: 400, message: "Not found" });
+    }
+    return res.status(200).json({
+      status: 200,
+      results,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({ status: 400, message: error.message });
+    }
+    return;
+  }
+});
 
 export default stockRouter;
