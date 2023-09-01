be6f1b0b03e71219461ad763c014a55b11b49cef
Dillon McQuade <dillonkmcquade@gmail.com>
2023-08-31 21:56:53 -0400

    finished transaction router

--- server/src/routes/transaction.ts
+++ server/src/routes/transaction.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { auth } from "express-oauth2-jwt-bearer";
-import { buyStock } from "../handlers/buyStock";
 import { sellStock } from "../handlers/sellStock";
-import UserController from "../controllers/UserController";
 import { pool } from "../index";
-import { StockController } from "../controllers/StockController";
+import UserController from "../models/UserController";
+import { StockController } from "../models/StockController";
+import { Transaction } from "../models/Transaction";
 
 const transactionRouter = Router();
 const userController = new UserController(pool);
@@ -15,13 +15,113 @@ const jwtCheck = auth({
   issuerBaseURL: process.env.AUTH0_DOMAIN,
 });
 
-transactionRouter
-  .use(jwtCheck)
-  .patch("/buy/:id", (req, res) =>
-    buyStock(req, res, userController, stockController),
-  )
-  .patch("/sell/:id", (req, res) =>
-    sellStock(req, res, userController, stockController),
-  );
+transactionRouter.use(jwtCheck);
+
+transactionRouter.patch("/buy/:id", async (req, res) => {
+  const { id } = req.params;
+  const auth = req.auth?.payload.sub as string;
+  const { quantity }: { quantity: number } = req.body;
+  if (!id || quantity === 0) {
+    return res.status(400).json({
+      status: 400,
+      data: req.body,
+      params: id,
+      message: "missing data",
+    });
+  }
+  try {
+    //fetch current price
+    const quote = await stockController.quote(id);
+    const currentPrice = quote["c"];
+    const amountToSubtract = Number(currentPrice) * quantity;
+
+    //get user balance so we can see if they have enough money
+    const balance = await userController.getBalance(auth);
+
+    // return undefined if user doesn't have enough money
+    if (balance < amountToSubtract) {
+      return res
+        .status(400)
+        .json({ status: 400, message: "Insufficient funds" });
+    }
+
+    //if user has enough money to make the transaction, continue
+    const transaction = new Transaction(id, quantity, currentPrice, auth);
+    await userController.insertTransaction(transaction);
+
+    // buy stock
+    const newBalance = await userController.updateBalance(
+      auth,
+      amountToSubtract,
+    );
+
+    // return error if insufficient funds
+    if (!newBalance) {
+      return res
+        .status(400)
+        .json({ status: 400, message: "Insufficient funds" });
+    }
+    return res.status(200).json({
+      status: 200,
+      message: "stock purchased successfully",
+      balance: newBalance,
+    });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: "Server error" });
+  }
+});
+
+transactionRouter.patch("/sell/:id", async (req, res) => {
+  const { id } = req.params;
+  const auth = req.auth?.payload.sub as string;
+  const { quantity }: { quantity: number } = req.body;
+  if (!id || quantity === 0) {
+    return res.status(400).json({
+      status: 400,
+      data: req.body,
+      params: id,
+      message: "missing data",
+    });
+  }
+  try {
+    //check if user has enough shares to sell desired amount
+    const holdings = await userController.getHoldings(auth);
+    const numOfShares = holdings.reduce((accumulator, currentValue) => {
+      if (currentValue.symbol === id) {
+        return accumulator + currentValue.quantity;
+      } else {
+        return accumulator + 0;
+      }
+    }, 0);
+
+    if (numOfShares < quantity) {
+      return res.status(404).json({
+        status: 404,
+        message: "You don't own enough shares to sell this much",
+      });
+    }
+
+    //fetch current price
+    const quote = await stockController.quote(id);
+    const currentPrice = quote["c"];
+
+    //insert transaction to DB
+
+    const transaction = new Transaction(id, -quantity, currentPrice, auth);
+    await userController.insertTransaction(transaction);
+
+    //update user balance, !!! amountToAdd must be negative
+    const amountToAdd = quantity * currentPrice;
+    const newBalance = await userController.updateBalance(auth, -amountToAdd);
+
+    return res.status(200).json({
+      status: 200,
+      message: "stock sold successfully",
+      balance: newBalance,
+    });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: "Server error" });
+  }
+});
 
 export default transactionRouter;
