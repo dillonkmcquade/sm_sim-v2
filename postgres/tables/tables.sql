48379edb4ca315190fa77c3d1d6fd100e873af6c
Dillon McQuade <dillonkmcquade@gmail.com>
2023-09-28 14:59:39 -0400

    change postgres from latest to 15.4

--- postgres/tables/tables.sql
+++ postgres/tables/tables.sql
@@ -1,5 +1,20 @@
 BEGIN TRANSACTION;
 
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  auth0_id VARCHAR(30) NOT NULL,
+  address VARCHAR(80),
+  telephone VARCHAR(15),
+  name VARCHAR(40),
+  nickname VARCHAR(40),
+  balance FLOAT NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  picture TEXT,
+  email VARCHAR(40),
+  watch_list TEXT[],
+  UNIQUE(auth0_id)
+);
+
 CREATE TABLE tickers (
   description TEXT,
   symbol TEXT NOT NULL,
@@ -16,19 +31,5 @@ CREATE TABLE transactions (
   REFERENCES users(auth0_id)
 );
 
-CREATE TABLE users (
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  auth0_id VARCHAR(30) NOT NULL,
-  address VARCHAR(80),
-  telephone VARCHAR(15),
-  name VARCHAR(40),
-  nickname VARCHAR(40),
-  balance FLOAT NOT NULL,
-  created_at TIMESTAMP NOT NULL,
-  picture TEXT,
-  email VARCHAR(40),
-  watch_list TEXT[],
-  UNIQUE(auth0_id)
-);
 
 COMMIT;
