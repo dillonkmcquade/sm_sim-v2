f3401f43d8c66d284358bbb246c55f8d55cc0dee
Dillon McQuade <dillonkmcquade@gmail.com>
2023-10-02 14:52:17 -0400

    add comments

--- server/src/services/DatabaseServiceModel.ts
+++ server/src/services/DatabaseServiceModel.ts
@@ -2,9 +2,14 @@ import { Pool, QueryConfig, QueryResultRow } from "pg";
 
 export abstract class DatabaseServiceModel<T extends QueryResultRow> {
   protected pool: Pool;
+
   constructor(pool: Pool) {
     this.pool = pool;
   }
+  /*
+   * This wrapper will allow tsserver to know the shape of the database rows being returned without
+   * manually inputting the generic each time.
+   */
   protected async query<R extends QueryResultRow = T>(
     queryString: string | QueryConfig<any[]>,
     args: any[] | undefined,
