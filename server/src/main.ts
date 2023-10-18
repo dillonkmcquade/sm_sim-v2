45c47ad595b0e65c0a6b913f272dfd6f5f70f639
Dillon McQuade <dillonkmcquade@gmail.com>
2023-10-18 18:16:40 -0400

    add swagger spec

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { writeFileSync } from 'node:fs';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
@@ -12,8 +13,10 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('MarketSim')
     .setDescription('The marketsim api')
+    .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+  writeFileSync('./openapi/swagger-spec.json', JSON.stringify(document));
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3001);
