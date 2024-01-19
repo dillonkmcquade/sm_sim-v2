2f8e6e10e5ba1a915bb441c7395d70d0aa93caf6
Dillon McQuade <dillonkmcquade@gmail.com>
2024-01-18 22:25:25 -0500

    fix compose file, add example .env

--- client/src/env.d.ts
+++ client/src/env.d.ts
@@ -1,10 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string;
   readonly VITE_SERVER_URL: string;
-  readonly VITE_POLYGON_KEY: string;
-  readonly VITE_FINNHUB_KEY: string;
   readonly VITE_AUTH0_DOMAIN: string;
   readonly VITE_AUTH0_CLIENT_ID: string;
   readonly VITE_AUTH0_AUDIENCE: string;
